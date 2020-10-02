34c783afbe836272344b58fa627d524daffed5e9
Jared Scarr <jaredscarr@gmail.com>
2020-10-02 14:00:01 -0700

    Added project route. Removed routes for deleted components.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Fragment } from 'react'
 import './App.css'
-import AppBar from './components/Navigation/AppBar'
-import Contact from './components/Home/Contact'
-import Experiments from './components/Projects/Experiments'
+import Experiments from './components/Projects/index'
 import Home from './components/Home/Home';
 import Terrain from './components/Projects/Terrain'
 import Horizon from './components/Projects/Horizon'
 import Sphere from './components/Projects/Sphere'
+import FloatSwarm from './components/Projects/FloatSwarm'
 import { Switch, Route } from "react-router-dom"
 
 const App = () => {
 
   return (
     <Fragment>
-      <AppBar />
       <Switch>
+        <Route path="/floatswarm" exact component={FloatSwarm} />
         <Route path="/horizon" exact component={Horizon} />
         <Route path="/sphere" exact component={Sphere} />
         <Route path="/terrain" exact component={Terrain} />
-        <Route path="/contact" exact component={Contact} />
         <Route path="/experiments" exact component={Experiments} />
         <Route path="/" exact component={Home} />
       </Switch>
