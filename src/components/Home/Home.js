f2d32809bc8ece02e19816d3187029e8b0d413cf
Jared Scarr <jaredscarr@gmail.com>
2020-10-05 16:51:36 -0700

    Form outline complete for Home page.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,23 @@
-import React, { Suspense, Fragment } from "react"
-import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons"
+import React, { Suspense, Fragment, useRef, useState } from "react"
+import { Parallax, ParallaxLayer } from "@react-spring/parallax"
 import { Canvas } from "react-three-fiber"
 import Background from './Background'
 import Experiments from '../Projects/index'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography'
+import FormControl from '@material-ui/core/FormControl'
+import FormGroup from '@material-ui/core/FormGroup';
+import Input from '@material-ui/core/Input'
+import InputLabel from '@material-ui/core/InputLabel'
+import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles'
 import Link from '@material-ui/core/Link'
 import Code from '@material-ui/icons/Code'
 import GitHub from '@material-ui/icons/GitHub'
 import LinkedIn from '@material-ui/icons/LinkedIn'
+import { useFormik } from 'formik'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -43,6 +50,17 @@ const useStyles = makeStyles((theme) => ({
   link: {
     margin: theme.spacing(2),
   },
+  formLayout: {
+    marginBottom: theme.spacing(4),
+  },
+  textFieldLayout: {
+    margin: theme.spacing(2),
+  },
+  submitButton: {
+    textAlign: 'center',
+    marginTop: theme.spacing(3),
+    // marginBottom: theme.spacing(4),
+  },
 }));
 
 const Copyright = () => {
@@ -58,10 +76,54 @@ const Copyright = () => {
   );
 }
 
+const validate = values => {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = 'Required';
+  } else if (values.name.length > 15) {
+    errors.name = 'Must be 15 characters or less';
+  }
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+
+  if (!values.message) {
+    errors.message = 'Required';
+  } else if (values.message.length > 500) {
+    errors.message = 'Must be 500 characters or less';
+  }
+
+  return errors;
+};
+
 const Home = () => {
   
   const classes = useStyles()
-  let parallax;
+  const ref = useRef()
+  const [formVisible, setFormVisibility] = useState(false)
+
+  // remove these blocks?
+  const [values, setValues] = React.useState({})
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('submit')
+  }
+
+  const formik = useFormik({
+     initialValues: {
+       name: '',
+       email: '',
+       message: '',
+     },
+     validate,
+     onSubmit: values => {
+       console.log(values)
+     },
+   })
 
   return (
     <Fragment>
@@ -70,34 +132,34 @@ const Home = () => {
           <Typography
             className={classes.navlink}
             variant="subtitle2"
-            onClick={() => parallax.scrollTo(0)}
+            onClick={() => ref.current.scrollTo(0)}
           >
             HOME
           </Typography>
           <Typography
             className={classes.navlink}
             variant="subtitle2"
-            onClick={() => parallax.scrollTo(1)}
+            onClick={() => ref.current.scrollTo(1)}
           >
             EXPERIMENTS
           </Typography>
           <Typography
             className={classes.navlink}
             variant="subtitle2"
-            onClick={() => parallax.scrollTo(3)}
+            onClick={() => ref.current.scrollTo(3)}
           >
             CONNECT
           </Typography>
         </Toolbar>
       </AppBar>
-      <Parallax pages={4} ref={ref => parallax = ref}>
+      <Parallax pages={4} ref={ref}>
         <div>
           <ParallaxLayer
             offset={0}
             speed={-1.1}
             factor={0.9}
             style={{ opacity: 0.3 }}
-            onClick={() => parallax.scrollTo(1)}
+            onClick={() => ref.current.scrollTo(1)}
           >
             <Suspense fallback="Loading ... ">
               <Canvas postition={[0, 0, 0]}>
@@ -119,7 +181,7 @@ const Home = () => {
             alignItems: "center",
             justifyContent: "center"
           }}
-          onClick={() => parallax.scrollTo(1)}
+          onClick={() => ref.current.scrollTo(1)}
         >
           <Typography className={classes.title} variant="h4" align="center" color="textPrimary" paragraph>
             jaredscarr.com
@@ -133,10 +195,48 @@ const Home = () => {
         <ParallaxLayer offset={1.1} speed={1} style={{ textAlign: "center" }}>
           <Experiments />
         </ParallaxLayer>
-        <ParallaxLayer offset={3.4} speed={0} style={{ textAlign: "center" }}>
-          <Typography className={classes.subtitle} variant="h6" align="center" color="textSecondary" paragraph>
-            jaredscarr@gmail.com
-          </Typography>
+        <ParallaxLayer offset={3.1} speed={0} style={{ textAlign: "center" }}>
+          <div className={classes.formLayout}>
+            <form
+              onSubmit={formik.handleSubmit}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="name"
+                className={classes.textFieldLayout}
+                label="Name"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.name}
+              />
+              {formik.touched.name && formik.errors.name ? (<div>{formik.errors.name}</div>) : null}
+              <TextField
+                id="email"
+                className={classes.textFieldLayout}
+                label="Email"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+              />
+              {formik.touched.email && formik.errors.email ? (<div>{formik.errors.email}</div>) : null}
+              <div>
+                <TextField
+                  id="message"
+                  className={classes.textFieldLayout}
+                  label="Message"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.message}
+                  multiline
+                />
+                {formik.touched.message && formik.errors.message ? (<div>{formik.errors.message}</div>) : null}
+              </div>
+              <div className={classes.submitButton}>
+                <Button type="submit" variant="outlined" >Submit</Button>
+              </div>
+            </form>
+          </div>
           <Typography className={classes.linkPosition} align="center">
             <Link
               color="inherit"
