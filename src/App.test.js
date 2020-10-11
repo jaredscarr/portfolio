2fa263aa3040f1f5a845e6cdd2b9cb4067d5f971
Jared Scarr <jaredscarr@gmail.com>
2020-10-11 15:26:43 -0700

    First basic tests added.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import 'mutationobserver-shim'
+import renderer from 'react-test-renderer'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { unmountComponentAtNode } from 'react-dom'
+import Home from './components/Home/Home'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+global.MutationObserver = window.MutationObserver;
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe('Home Component', () => {
+  test('Home Renders correctly', () => {
+    const tree = renderer.create(<Router><Home /></Router>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+})
+
--- src/components/Home/ContactForm.test.js
+++ src/components/Home/ContactForm.test.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import renderer from 'react-test-renderer';
+import React from 'react'
+import 'mutationobserver-shim'
+import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from 'react-dom/test-utils'
 import ContactForm from './ContactForm'
 
-describe('Contact Form', () => {
-  test('Contact Form Renders correctly', () => {
-    const tree = renderer.create(<ContactForm />).toJSON();
-    expect(tree).toMatchSnapshot();
+global.MutationObserver = window.MutationObserver;
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+
+it("Renders", () => {
+  act(() => {
+    render(<ContactForm />, container);
   });
+  expect(container.textContent).toBe("NameEmailMessageSubmit");
+});
