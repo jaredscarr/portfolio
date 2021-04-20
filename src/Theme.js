2cc0ec9842e25ca3e3f0ccbdd97bc599ff5d22c3
Jared <jaredscarr@gmail.com>
2021-04-19 17:26:46 -0700

    Add theme for Material-ui ThemeProvider.

---
+++ src/Theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      light: '#e2f1f8',
+      main: '#b0bec5',
+      dark: '#808e95',
+    },
+    secondary: {
+      light: '#ffffff',
+      main: '#e0e0e0',
+      dark: '#aeaeae',
+    }
+  }
+});
