e18ef21089c39770ebf7adf92de0d8d4ca9b993d
jared <jaredscarr@gmail.com>
2021-05-29 18:34:00 -0700

    update packages and rmove codesandbox. Spacing updated to use Grid component.

--- src/components/Home/ExternalLinks.js
+++ src/components/Home/ExternalLinks.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 
-import Code from '@material-ui/icons/Code'
 import GitHub from '@material-ui/icons/GitHub'
 import LinkedIn from '@material-ui/icons/LinkedIn'
 import { Link as MuiLink } from '@material-ui/core'
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 
@@ -13,7 +13,7 @@ import getTheme from '../../Theme'
 
 const ExternalLinks = ({ darkState, partyState }) => {
   const paletteType = darkState ? 'dark' : 'light'
-    const themeType = partyState ? 'party' : 'default'
+  const themeType = partyState ? 'party' : 'default'
   const theme = getTheme(paletteType, themeType)
   const route = useLocation();
   let background = 'inherit';
@@ -25,56 +25,59 @@ const ExternalLinks = ({ darkState, partyState }) => {
   const useStyles = makeStyles((theme) => ({
     root: {
       backgroundColor: background,
-      paddingTop: '10vh',
-    },
-    linkPosition: {
-      marginTop: theme.spacing(5),
+      // paddingTop: '10vh',
     },
     link: {
-      margin: theme.spacing(2),
+      // margin: theme.spacing(2),
     },
   }));
   
   const classes = useStyles()
-
+  
   return (
     <div className={classes.root}>
-      <Typography
-        className={classes.linkPosition}
-        align="center"
-        color="textSecondary"
-      > 
-        <MuiLink
-          color="inherit"
-          variant="inherit"
-          className={classes.link}
-          href="https://www.linkedin.com/in/jaredscarr"
-          target="_blank"
-          rel="noreferrer moopener"
-        >
-          <LinkedIn />
-        </MuiLink>
-        <MuiLink
-          color="inherit"
-          variant="inherit" 
-          className={classes.link}
-          href="https://codesandbox.io/u/jaredscarr"
-          target="_blank"
-          rel="noreferrer moopener"
-        >
-          <Code />
-        </MuiLink>
-        <MuiLink
-          color="inherit"
-          variant="inherit"
-          className={classes.link}
-          href="https://github.com/jaredscarr"
-          target="_blank"
-          rel="noreferrer moopener"
-        >
-          <GitHub />
-        </MuiLink>
-      </Typography>
+      <Grid
+        container
+        spacing={10}
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item>
+          <MuiLink
+            color="inherit"
+            variant="inherit"
+            className={classes.link}
+            href="https://www.linkedin.com/in/jaredscarr"
+            target="_blank"
+            rel="noreferrer moopener"
+          >
+            <Typography
+              align="center"
+              color="textSecondary"
+            > 
+              <LinkedIn />
+            </Typography>
+          </MuiLink>
+        </Grid>
+        <Grid item>
+          <MuiLink
+            color="inherit"
+            variant="inherit"
+            className={classes.link}
+            href="https://github.com/jaredscarr"
+            target="_blank"
+            rel="noreferrer moopener"
+          >
+            <Typography
+              align="center"
+              color="textSecondary"
+            >
+              <GitHub />
+            </Typography>
+          </MuiLink>
+        </Grid>
+      </Grid>
     </div>
   );
 }
